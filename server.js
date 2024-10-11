8b8e750677878c09c878b6eb767ad7bfd580a9ec
manuel28s <manuel.samaniego28sep1973@gmail.com>
2024-10-11 11:30:59 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.end('Estudiantes de nuevo ingreso:
+Enfócate en la emoción de empezar una nueva etapa y las oportunidades que les esperan.
+Destaca la importancia de la formación técnica y su impacto en el futuro profesional.
+Motívalos a aprovechar al máximo los recursos que el ITSE les ofrece.
+Porque todos somos ITSE');
 });
 
 server.listen(port, () => {
